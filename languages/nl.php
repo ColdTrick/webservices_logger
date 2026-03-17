d10ea269fd6f36d202077c1a8e396807e5c9551c
Jerôme Bakker <jeabakker@coldtrick.com>
2026-03-17 14:56:01 +0100

    chore: added Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file was created by Translation Editor v14.0.4
+ * On 2026-03-17 14:55
+ */
+
+return array (
+  'collection:object:webservice_log_entry' => 'Webservice log regels',
+  'item:object:webservice_log_entry' => 'Webservice log regel',
+  'webservices_logger:settings:retention' => 'Behoud de logs voor x dagen',
+  'webservices_logger:settings:retention:help' => 'Na deze termijn zullen de logs worden verwijderd',
+  'webservices_logger:settings:log_type' => 'Welke API verzoeken moeten worden gelogd',
+  'webservices_logger:settings:log_type:help' => 'Verander dit om het aantal log regels te limiteren',
+  'webservices_logger:settings:log_type:all' => 'Alle verzoeken',
+  'webservices_logger:settings:log_type:error' => 'Alleen verzoeken die resulteerden in een fout',
+  'webservices_logger:settings:log_type:none' => 'Geen verzoeken',
+  'webservices_logger:menu:admin_header:logs' => 'Logs',
+  'webservices_logger:menu:entity:api_key:inspect' => 'Bekijk de logs',
+  'admin:configure_utilities:ws_logs' => 'Webservices logs',
+  'table_columns:fromProperty:api_method' => 'API methode',
+  'table_columns:fromView:api_success' => 'Succes',
+  'table_columns:fromView:api_error_message' => 'Foutmelding',
+  'table_columns:fromView:api_details' => 'Details',
+  'webservices_logger:log_entry:success' => 'Succes',
+  'webservices_logger:log_entry:error' => 'Fout',
+  'webservices_logger:details:request' => 'Verzoek',
+  'webservices_logger:details:request:time_created' => 'Verzoek starttijd',
+  'webservices_logger:details:request:request_method' => 'Verzoek methode',
+  'webservices_logger:details:request:view_type' => 'Output formaat',
+  'webservices_logger:details:request:user_agent' => 'User agent',
+  'webservices_logger:details:request:response_time' => 'Antwoordtijdstip',
+  'webservices_logger:details:request:result_code' => 'API resultaat code',
+  'webservices_logger:details:request:api_key' => 'Gebruikte API sleutel',
+  'webservices_logger:details:get' => 'GET',
+  'webservices_logger:details:get:none' => 'Geen GET parameters aangeleverd',
+  'webservices_logger:details:post' => 'POST',
+  'webservices_logger:details:post:none' => 'Geen POST parameters aangeleverd',
+  'webservices_logger:forms:admin:filter:created_after' => 'Aangemaakt na',
+  'webservices_logger:forms:admin:filter:created_before' => 'Aangemaakt voor',
+  'webservices_logger:forms:admin:filter:api_key' => 'API sleutel',
+);
